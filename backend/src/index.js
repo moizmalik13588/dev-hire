4a69640a2b1ce3c3dcae9653d825c49fe1e99fcc
Moiz Malik <your_email@example.com>
2026-04-14 21:51:14 +0500

    fix: dynamic API URL via env variable

--- backend/src/index.js
+++ backend/src/index.js
@@ -16,7 +16,15 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(helmet());
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://dev-hire-beta.vercel.app/", // apna Vercel URL daalo
+    ],
+    credentials: true,
+  }),
+);
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
